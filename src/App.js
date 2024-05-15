9755532a8772a19d63ea2c2feedde2a5875579ad
RV <vrahavi0112@gmail.com>
2024-05-15 03:27:33 -1000

    uploaded

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import Todo from './components/Todo.jsx'
+//import Todoitems from './components/Todoitems.jsx'
+import './App.css';
+
+function App() {
+  return (
+  <Todo/>
+  );
+}
+
+export default App;
